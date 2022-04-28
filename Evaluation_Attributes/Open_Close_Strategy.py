db439f536b012aad4e279bb502e76c03bf5761db
AliDajmar <ali.dajmar@gmail.com>
2022-04-28 14:54:07 +0430

    Updating OS_CS: Correction in find_dates. Adding find_prices.

--- Evaluation_Attributes/Open_Close_Strategy.py
+++ Evaluation_Attributes/Open_Close_Strategy.py
@@ -2,52 +2,58 @@ import numpy as np
 import pandas as pd
 import datetime
 
+
 class OS_CS:
 
-    def __init__(self, trading_data:pd.DataFrame)-> None:
-        """
-        Parameters
-        ----------
-        trading_data : pd.DataFrame
-            DESCRIPTION.
+    def __init__(self, trading_data: pd.DataFrame, symbol: str) -> None:
+        self.symbol = symbol
+        # Renaming Time columns
+        self.trading_data = pd.concat([trading_data.iloc[:, :2].rename(columns={'Time': 'Time1'}),
+                                      trading_data.iloc[:, 2:].rename(columns={'Time': 'Time2'})], axis=1)
 
-        Returns
-        -------
-        None
-            DESCRIPTION.
-        """        
-        df1 = trading_data.iloc[:, 0:2].rename({"Time":"Time1"}, axis="columns")
-        df2 = trading_data.iloc[:, 2:].rename({"Time":"Time2"}, axis="columns")
-        Time = pd.concat([df1.Time1, df2.Time2],axis = 1)
-        self.Time = Time
-        self.trading_data = trading_data
+        # Removing Balance rows (price and time is 0)
+        self.trading_data = self.trading_data.drop(
+            index=trading_data[trading_data.Type == "Balance"].index).reset_index(drop=True)
 
-       
-    def find_open_dates(self)-> np.ndarray:
-        """
-        Returns
-        -------
-        np.ndarray
-            DESCRIPTION.
-        """
-        duration = self.Time.apply(lambda x: x["Time2"] - x["Time1"] if x["Time2"] !=0 else datetime.timedelta() , axis=1)
-        open_time_simulated = np.empty((len(duration),10),dtype=object) 
-        r = [-0.5,-0.4,-0.3,-0.2,-0.1, 0.1, 0.2, 0.3, 0.4, 0.5]
-        for i in range(10):
-            open_time_simulated[:,i] = self.Time.Time1 + duration * r[i]
-        return open_time_simulated
+        # Correction of Time2 datatype
+        self.trading_data.Time2 = pd.to_datetime(self.trading_data.Time2)
 
-    def find_close_dates(self)-> np.ndarray:
-        """
-        Returns
-        -------
-        np.ndarray
-            DESCRIPTION.
-        """
-        duration = self.Time.apply(lambda x: x["Time2"] - x["Time1"] if x["Time2"] !=0 else 0 , axis=1)
-        close_time_simulated = np.empty((len(duration),10),dtype=object) 
-        r = [-0.5,-0.4,-0.3,-0.2,-0.1, 0.1, 0.2, 0.3, 0.4, 0.5]
-        for i in range(10):
-            close_time_simulated[:,i] = self.Time.Time2 + duration * r[i]
-        return close_time_simulated
-    
+    def find_dates(self, open_strategy: bool) -> pd.DataFrame:
+        r = [-0.5, -0.4, -0.3, -0.2, -0.1, 0.1, 0.2, 0.3, 0.4, 0.5]
+        duration = self.trading_data.apply(lambda x: x.Time2 - x.Time1, axis=1)
+        simulated_time = np.empty((len(duration), 10), dtype=object)
+        if open_strategy:
+            for i in range(10):
+                simulated_time[:, i] = self.trading_data.Time1 + duration * r[i]
+        else:
+            for i in range(10):
+                simulated_time[:, i] = self.trading_data.Time2 + duration * r[i]
+
+        return pd.DataFrame(simulated_time)
+
+    def find_prices(self, simulated_times: pd.DataFrame) -> pd.DataFrame:
+        try:
+            price_df = pd.read_hdf('./price-data/'+self.symbol+'.h5')
+        except:
+            print("No data!")
+            return None
+
+        # Rounding datetime in simulated_times to the nearest minute, removinf seconds
+        simulated_times = simulated_times.applymap(
+            lambda x: x.floor('Min') if x.second < 30 else x.ceil('Min'))
+
+        # Get price for simulated data
+        return simulated_times.applymap(lambda x: price_df.loc[x].values[0])
+
+    def main(self, open_strategy: bool) -> list:
+        # This is the wrapper function. It should be called at __init__ to run all steps
+
+        # Step1: Find simulated dates
+        simulated_time = self.find_dates(open_strategy)
+
+        # Step 2: Find prices for simulated dates
+        simulated_prices = self.find_prices(simulated_time)
+
+        # Step3: Calculate ranking of every trade between all simulated scenarios
+
+        # Step4: Calculate final score (ranking) for the trader
