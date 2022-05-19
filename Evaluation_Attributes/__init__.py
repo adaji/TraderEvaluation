f2c67b3d29d86d54f6fd6a32ec33a222b2a87433
mtrshirazi21 <mtrshirazi21@gmail.com>
2022-05-19 18:27:08 +0430

    modified Evaluation_Attributes/__init__.py

--- Evaluation_Attributes/__init__.py
+++ Evaluation_Attributes/__init__.py
@@ -1,2 +1,4 @@
 from .__Duration_Consistency import Duration
-from .__Positive_Negative_Return_Consistency import R_plus_minus
+from .__Positive_Negative_Return_Consistency import R_plus_minus 
+from .__Capacity import Capacity 
+from .__Loss_Aversion import Loss_Aversion
