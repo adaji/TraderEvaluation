fd42a92cc071de61a73dc83d90f2ecaf67842e10
mtrshirazi21 <mtrshirazi21@gmail.com>
2022-05-25 20:35:30 +0430

    updata __init__ file and TestNote

--- Evaluation_Attributes/__init__.py
+++ Evaluation_Attributes/__init__.py
@@ -2,3 +2,10 @@ from .__Duration_Consistency import Duration
 from .__Positive_Negative_Return_Consistency import R_plus_minus 
 from .__Capacity import Capacity 
 from .__Loss_Aversion import Loss_Aversion
+from .__Risk_Adjustment import Risk_Adjusment
+from .__Risk_Stability import Risk_Stability
+from .__Experience import Experience
+from .__Open_Close_Strategy import OS_CS
+from .__Market_Correlation import Market_Correlation
+
+# TODO : __Open_Close_Strategy , __Risk_Stability
